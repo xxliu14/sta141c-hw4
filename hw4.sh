705aed9e8feadefb3aad7a323939d278d9b7819c
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-14 15:13:25 -0800

    Idontremember

--- hw4.sh
+++ hw4.sh
@@ -2,11 +2,47 @@ DATAFILE="/scratch/transaction.zip"
 # For testing from head node:
 #DATAFILE="/group/staclassgrp/transaction.zip"
 
-# Example:
-unzip -p ${DATAFILE} | 
-    head |
-    cat > result.txt
+#DATAFILE="/group/staclassgrp/transaction.zip"
 
+# 1.1
+unzip -p ${DATAFILE} |
+	head -1 |
+	tr ',' '\n' |
+	nl |
+	cat > colname_index.txt
+
+# 1.2
+unzip -p ${DATAFILE} |
+	wc -L |
+	cat > maxchars.txt
+
+# 1.3
+# ref: https://ss64.com/bash/grep.html
 unzip -p ${DATAFILE} | 
-    head -n 2 |
-    cat > result2.txt
+	grep -i "bicycle" | 
+	nl |
+	cat > bicycle.csv
+
+
+# 1.4
+#ref:https://stackoverflow.com/questions/19602181/how-to-extract-one-column-of-a-csv-file
+# found 18th col
+unzip -p ${DATAFILE} | 
+	cut -d ','  -f18 |
+	sort -n |
+	uniq |
+	cat > funding_agencies_set.txt
+
+#1.5
+# obli: col 8, decri: col 25
+unzip -p ${DATAFILE} | 
+	awk -F "," '{print $25, " : ", $8}' |
+	sort -k8 -nr |
+	head |
+	cat > largest.csv
+ 
+
+
+
+
+
