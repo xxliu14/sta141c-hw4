d38875c446689a83b6981e88a2d6c5a0f93fc661
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-14 15:05:24 -0800

    -Ihatewindows

--- submit.sh
+++ submit.sh
@@ -3,11 +3,11 @@
 # Use the staclass partition. Only applies if you are in STA141C
 #SBATCH --partition=staclass
 
-# Use two cores to get some pipeline parallelism
+# Use two cores to get some pipeline parallelism MEOW
 #SBATCH --cpus-per-task=2
 
 # Give the job a name
-#SBATCH --job-name=hw4
+#SBATCH --job-name=hw4_from_a_sad_windows_user
 
 # #SBATCH --mail-type=END
 # #SBATCH --mail-user=
