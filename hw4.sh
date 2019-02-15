c23fa4ea749975e5f6e8a65aa13762ef481453dd
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-14 19:12:35 -0800

    a

--- hw4.sh
+++ hw4.sh
@@ -37,8 +37,8 @@ unzip -p ${DATAFILE} |
 TOT_OB=8
 DESCRIPTION=25
 unzip -p ${DATAFILE} | 
-	cut -d ',' -f{TOT_OB,DESCRIPTION} |
-	sort -k{TOT_OB} -nr |
+	cut -d ',' -f ${TOT_OB,DESCRIPTION} |
+	sort -k ${TOT_OB} -nr |
 	head |
 	cat > largest.csv
 
--- q5.sh
+++ q5.sh
@@ -1,9 +1,9 @@
 # 1.5
 DATAFILE="/scratch/transaction.zip"
-TOT_OB=8
-DESCRIPTION=25
-unzip -p ${DATAFILE} | 
-	cut -d ',' -f ${TOT_OB,DESCRIPTION} |
-	sort -k ${TOT_OB} -nr |
-	head |
-	cat > largest.csv
+# 1.1
+# ref: https://ss64.com/bash/tr.html
+unzip -p ${DATAFILE} |
+	head -n 1 |
+	tr ',' '\n' |
+	nl |
+	cat > colname_index.txt
