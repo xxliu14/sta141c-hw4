c76d96ce990751734814226b7cc958edb9cd179e
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-14 18:52:36 -0800

    a

---
+++ q5.sh
@@ -0,0 +1,9 @@
+# 1.5
+DATAFILE="/scratch/transaction.zip"
+TOT_OB=8
+DESCRIPTION=25
+unzip -p ${DATAFILE} | 
+	cut -d ',' -f${TOT_OB,DESCRIPTION} |
+	sort -k${TOT_OB} -nr |
+	head |
+	cat > largest.csv
