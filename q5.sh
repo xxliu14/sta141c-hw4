0d51c12a93e4085d5d42e19ffdb4fd7902f9fd6d
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-14 18:55:42 -0800

    a

--- q5.sh
+++ q5.sh
@@ -3,7 +3,7 @@ DATAFILE="/scratch/transaction.zip"
 TOT_OB=8
 DESCRIPTION=25
 unzip -p ${DATAFILE} | 
-	cut -d ',' -f${TOT_OB,DESCRIPTION} |
-	sort -k${TOT_OB} -nr |
+	cut -d ',' -f ${TOT_OB,DESCRIPTION} |
+	sort -k ${TOT_OB} -nr |
 	head |
 	cat > largest.csv
