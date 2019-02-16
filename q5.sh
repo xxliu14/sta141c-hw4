766ed8174e4cbdb0f3c97b41805db6c8686aad09
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-16 01:17:01 -0800

    -ewww

--- q5.sh
+++ q5.sh
@@ -1,9 +1,9 @@
 DATAFILE="/scratch/transaction.zip"
 #2.4
-REC_NAME=51
+REC_ID=15
 unzip -p ${DATAFILE} | 
-	cut --delimiter=, --fields=${REC_NAME} |
-	sort |
+	cut --delimiter=, --fields=${REC_ID} |
+	sort --numeric |
 	uniq --count |
 	sort --reverse |
 	head -30 |
