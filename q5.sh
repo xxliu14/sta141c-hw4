a5b94d2fdd8e73a21f28e7775928acaf48228739
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-16 00:43:29 -0800

    -ewww

--- q5.sh
+++ q5.sh
@@ -1,16 +1,10 @@
 DATAFILE="/scratch/transaction.zip"
-#1.5
-# ref: https://ss64.com/bash/sort.html
-# total obligation: col 8, decription: col 25
-TOT_OBLIGATION=8
-DESCRIPTION=25
+#2.4
+REC_NAME=51
 unzip -p ${DATAFILE} | 
-	cut --delimiter=, --fields=${TOT_OBLIGATION},${DESCRIPTION} |
-	awk '$2!=""' |
-	sort --field-separator=, --key=1,1 --reverse --numeric-sort |
-	head -n50 |
-	cat > largest50.csv
-# find unique values
-cat largest50.csv | 
-	uniq |
-	cat > unique_large.csv
+	cut --delimiter=, --fields=${REC_NAME} |
+	sort |
+	uniq --count |
+	sort --reverse |
+	head -30 |
+	cat > most_recv.csv
