163483917e4515d9cefb94e30df82ed1643b72d8
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-26 17:00:42 -0800

    -meow?

--- hw5.sh
+++ hw5.sh
@@ -1,14 +1,13 @@
 DATAFILE='/scratch/transaction.zip'
 
-# subset the needed columns
-parent_recipient_id=52
-action_date=3
-total_obligation=8
+PARENT_RECIPIENT_ID=52
+ACTION_DATE=3
+TOTAL_OBLIGATION=8
 # ref: https://ss64.com/bash/sort.html
 unzip -p ${DATAFILE} | 
-	cut --delimiter=, --fields=${parent_recipient_id},${action_date},${total_obligation} |
-    sort -u |
-	cat > rm_duplicate.csv
+	cut --delimiter=, --fields=${PARENT_},${ACTION_DATE},${TOTAL_OBLIGATION} |
+    sort --uniq |
+	cat > duplicate_rm_3col.csv
 
-
+#paste -d, digit1.csv temp2.csv
 
