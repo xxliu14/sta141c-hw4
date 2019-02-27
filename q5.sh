542626329f8e8448f7e0265ee8b6095cfe28ee62
Xiaoxi Liu <xixliu@ucdavis.edu>
2019-02-26 19:12:05 -0800

    -meow?

--- q5.sh
+++ q5.sh
@@ -1,21 +1,4 @@
-FIRST_DIGIT=1
-TOTAL_OBLIGATION2=2
-PARENT_RECIPIENT_ID2=3
-# extract id column
-cat duplicate_rm_3col.csv |
-    cut --delimiter=, --fields=${PARENT_RECIPIENT_ID2} | 
-    cat > rec_id.csv
-
-# extract obligation column 
-cat duplicate_rm_3col.csv |
-    cut --delimiter=, --fields=${TOTAL_OBLIGATION2} | 
-    cat > obli.csv
-
-# extract first digit
-cat obli.csv |
-    cut --characters=${FIRST_DIGIT} |
-    cat > digit1.csv
-
-# combine to one file
-paste --delimiters=, digit1.csv rec_id.csv |
-    cat > rec_digit.csv
+cat rec_digit.csv | 
+    awk -F, '$1!="0"' |
+    awk -F, '$1!="-"' |
+    cat > modified_rec_digit.csv 
